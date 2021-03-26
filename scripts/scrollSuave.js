07a5912d64df62853407ab45da85c96d36c5bfdb
Lucas Fernandes <Lucas_201130@hotmail.com>
2021-03-26 20:22:58 -0300

    algumas mudancas

--- scripts/scrollSuave.js
+++ scripts/scrollSuave.js
@@ -1,20 +1,15 @@
 export default function scroll() {
     const menuLinks = document.querySelectorAll('[data-menu="links"] li');
+    const menuConteudo = document.querySelectorAll('[data-menu="conteudo"]')
 
-    if (!menuLinks) {
-        menuLinks.forEach((lista) => {
-            lista.addEventListener('click', scrollView);
+    menuLinks.forEach((link, posicao) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const scroll = menuConteudo[posicao].offsetTop
+            window.scrollTo({
+                top: scroll,
+                behavior: 'smooth'
+            })
         })
-
-        function scrollView(e) {
-            e.preventDefault();
-            const item = e.target.getAttribute('href');
-            const selecionar = document.querySelector(item);
-
-            selecionar.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-            });
-        }
-    }
+    })
 }
