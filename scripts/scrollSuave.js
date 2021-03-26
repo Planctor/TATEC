8f0faa023465f70a82b938f8c7454bb168b60551
Lucas Fernandes <Lucas_201130@hotmail.com>
2021-03-26 16:21:44 -0300

    ultima att

--- scripts/scrollSuave.js
+++ scripts/scrollSuave.js
@@ -1,18 +1,20 @@
 export default function scroll() {
-    const menuLinks = document.querySelectorAll('[data-menu="links"] li')
+    const menuLinks = document.querySelectorAll('[data-menu="links"] li');
 
-    menuLinks.forEach((lista) => {
-        lista.addEventListener('click', scrollView)
-    })
-
-    function scrollView(e) {
-        e.preventDefault()
-        const item = e.target.getAttribute('href')
-        const selecionar = document.querySelector(item)
-
-        selecionar.scrollIntoView({
-            behavior: "smooth",
-            block: "center",
+    if (!menuLinks) {
+        menuLinks.forEach((lista) => {
+            lista.addEventListener('click', scrollView);
         })
+
+        function scrollView(e) {
+            e.preventDefault();
+            const item = e.target.getAttribute('href');
+            const selecionar = document.querySelector(item);
+
+            selecionar.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+            });
+        }
     }
 }
