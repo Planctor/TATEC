cf68083fbd0312a8ac4df154bd25e15d1a93590f
Lucas Fernandes <Lucas_201130@hotmail.com>
2021-03-27 15:02:21 -0300

    adicionando mais animacoes

--- scripts/scrollview.js
+++ scripts/scrollview.js
@@ -10,5 +10,21 @@ export default function view() {
                 }
             })
         })
+
+        const animaMutation = document.querySelectorAll('[data-anima]')
+        const animaArray = Array.from(animaMutation)
+        animaArray.forEach((data) => {
+            function observador() {
+                const filhos = data.children
+                data.classList.add('animacao')
+                filhos[0].classList.add('animacaoEsquerda')
+                filhos[1].classList.add('animacaoDireita')
+
+                mutacao.disconnect()
+            }
+            const mutacao = new MutationObserver(observador)
+            return mutacao.observe(data, { attributes: true })
+        })
+
     }
 }
